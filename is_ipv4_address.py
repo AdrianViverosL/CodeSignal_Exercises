855537dcff5efa9cd8840711de15263c637399a3
AdsLuqs <aluques12@gmail.com>
2021-03-06 01:42:58 -0600

    is_ipv4_address almost finished, only once case is not fullfiled yet

--- is_ipv4_address.py
+++ is_ipv4_address.py
@@ -1,6 +1,14 @@
 def isIPv4Address(inputString):
     x = inputString.split('.')
-    x = [int(i) for i in x if i != '']
+    x = [item for item in x if (len(item)>1 and item[0] != '0') or len(item) == 1]
+    #print(x)
+    
+    try:
+        x = [int(i) for i in x if i != '']
+
+    except ValueError:
+        return False
+    
     if(len(x) != 4):
         return x, False
     
@@ -9,8 +17,11 @@ def isIPv4Address(inputString):
             return x, False
 
     return x, True
+    
+    #return len(x) == 4 and all(n.isdigit() and 0 <= int(n) < 256 for n in x )
 
-#print(isIPv4Address("172.16.254.1")) #True
+print(isIPv4Address("172.16.254.1")) #True
 #print(isIPv4Address("172.316.254.1")) #False 316 is not in the range 0, 255
 #print(isIPv4Address(".254.255.0")) #False, first number missing
 print(isIPv4Address("01.233.161.131")) 
+print(isIPv4Address("0..1.0.1")) 
